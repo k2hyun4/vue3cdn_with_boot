8aecf4596f519bcff3473e99ec8e3f423e46efac
khkim <khkim@gaia3d.com>
2021-12-30 13:21:31 +0900

    init project

---
+++ src/test/java/com/example/prac_vue_router/PracVueRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prac_vue_router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracVueRouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
